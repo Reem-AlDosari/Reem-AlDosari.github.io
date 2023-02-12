91fd608ae2527b0723d1740e7ac295b635ebe200
reem282 <reem.al.dosari@outlook.com>
2023-02-13 01:13:23 +0300

    try

--- Website/projects/script.js
+++ Website/projects/script.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
+       // if (window.scrollY > 60) {
+        //    document.querySelector('#scroll-top').classList.add('active');
+       // } else {
+        //    document.querySelector('#scroll-top').classList.remove('active');
+       // }
     });
 });
 
